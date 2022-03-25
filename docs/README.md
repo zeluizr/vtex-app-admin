56a7c569b676775fd9ffdceacc948fa226aec846
Jose Luiz Rodrigues <zeluizr@me.com>
2022-03-24 23:22:32 -0300

    eshopper app

--- docs/README.md
+++ docs/README.md
@@ -1,25 +1,3 @@
-# [Deprecated] Admin Example
+# Eshopper Score Rankings
 
-_We're working on our new design system. Please get in touch if you are interested._
-
-An example admin app that adds a menu button to the admin sidebar and a navigation via parameter example.
-
-### How to develop admins
-
-1. Admins always declare routes in `/admin/app/<route>`
-
-2. Declare the `admin` builder in your manifest
-
-3. When installed, the user navigates to `/admin/<route>`, but your app runs in an iframe that points to `/admin/app/<route>`.
-
-4. You can develop directly in the `/admin/app` route for convenience, but don't forget to test it inside the iframe. :)
-
-### Quickstart
-
-1. Clone this repo
-
-2. `yarn --cwd react/` for code completion
-
-3. `vtex link`
-
-4. Navigate to `workspace--account.myvtex.com/admin/app/example`
+The Eshopper score is an all-in-one metric built from expert analysis of a site's usability and shopper friendliness. It's built from 8 components: SEO, Navigation, Content & Customization, Web Performance, Purchase Policy, Cart & Checkout, Omnichannel y Customer Service
--- react/EshopperApp.tsx
+++ react/EshopperApp.tsx
@@ -1,23 +1,76 @@
-import React, { FC } from 'react'
-import { FormattedMessage } from 'react-intl'
-import { Layout, PageBlock, PageHeader } from 'vtex.styleguide'
-import './styles.global.css'
+import React, { useEffect, useState } from 'react';
+import { Box, Layout, PageHeader, Progress, Tag, Totalizer } from 'vtex.styleguide';
+import './styles.global.css';
+
+const TITLE = "Eshopper Score Rankings";
+const SUBTITLE = "The Eshopper score is an all-in-one metric built from expert analysis of a site's usability and shopper friendliness. It's built from 8 components: SEO, Navigation, Content & Customization, Web Performance, Purchase Policy, Cart & Checkout, Omnichannel y Customer Service";
+
+type SiteInfoType = {
+  title?: string,
+  vertical?: string,
+}
+
+export default function EshopperApp() {
+  const [loading, setLoading] = useState(true)
+  const [siteInfo, setSiteInfo] = useState<SiteInfoType>()
+
+  useEffect(() => {
+    (async () => {
+      const { Results } = await fetch("https://api.builtwith.com/v19/api.json?KEY=2a15b1b8-cc56-4637-8d0e-a230095acb79&LOOKUP=fravega.com").then(res => res.json())
 
 
-const EshopperApp: FC = () => {
+      setSiteInfo({
+        title: Results[0]?.Meta?.CompanyName,
+        vertical: Results[0]?.Meta?.Vertical
+      })
+
+      setLoading(false);
+    })()
+  })
+
   return (
-    <Layout
-      pageHeader={
-        <PageHeader
-          title={<FormattedMessage id="admin-example.hello-world" />}
-        />
-      }
-    >
-      <PageBlock variation="full">
-        <h1>Hello</h1>
-      </PageBlock>
+    <Layout pageHeader={<PageHeader title={TITLE} subtitle={SUBTITLE} />}>
+      <Box>
+        {loading ? <Progress type="steps" steps={['inProgress']} /> : (
+          <div>
+            <section className='justify-between flex items-center'>
+              <h2>{siteInfo?.title}</h2>
+              <Tag>
+                Category: {siteInfo?.vertical}
+              </Tag>
+            </section>
+            <section>
+              <Totalizer
+                items={[
+                  {
+                    inverted: true,
+                    value: 'Total Orders',
+                    label: 'Until 10h10',
+                  },
+                  {
+                    label: 'Today',
+                    value: 12364,
+                  },
+                  {
+                    label: 'Yesterday',
+                    value: 11980,
+                  },
+
+                  {
+                    label: 'Last Week',
+                    value: 10776,
+                  },
+                  {
+                    label: 'Last Year',
+                    value: 9802,
+                  },
+                ]}
+              />
+            </section>
+          </div>
+        )}
+
+      </Box>
     </Layout>
   )
 }
-
-export default EshopperApp
