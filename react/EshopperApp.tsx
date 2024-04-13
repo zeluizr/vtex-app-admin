8d31969b8052a400f3bf96537cf61bafae486649
Jose Luiz Rodrigues <zeluizr@me.com>
2024-04-12 20:19:40 -0400

    Fix eslint configuration in .vscode/settings.json and remove unused import in react/EshopperApp.tsx

--- react/EshopperApp.tsx
+++ react/EshopperApp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   BarChart, Layout, PageBlock, PageHeader,
   Spinner, Table, Tag, Totalizer
